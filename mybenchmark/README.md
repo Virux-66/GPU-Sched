8b586de3e5de02e6a38f009fc8400084ed7ef130
Virux Zheng <viruxzheng@gmail.com>
2022-12-13 22:58:51 +0800

    68

--- mybenchmark/README.md
+++ mybenchmark/README.md
@@ -21,17 +21,34 @@ In this document, the specifications of all benchmarks used in this system are d
   - arithmetic intensity: 18.149
   - type: bandwidth-bound
 
+  `${CUDA_HOME}/samples/0_Simple/matrixMul`:
+  - data size: -wA=-hA=-wB=-hB=1024
+  - Launch Stats: 1024 blocks of 1024 threads 
+  - Memory footprint: 12 MB
+  - Memory traffic: 11.76 MB
+  - fp operation: 2,147,483,648
+  - arithmetic intensity: 174.149
+  - type: compute-bound
+
 
 
 - `${CUDA_HOME}/samples/4_Finance/binomialOptions`
   - data size: `MAX_OPTIONS`= 1024
   - Launch stats: 1024 blocks of 128 threads
   - Memory footprint: ~24 KB
-  - Memory traffic:  ~44..4 KB
+  - Memory traffic:  ~44.4 KB
   - fp operation: 6,511,611,904
   - arithmetic intensity: 143220
   - type: compute-bound
 
+- `${CUDA_HOME}/sample/4_Finance/binomialOptions`
+  - data size: `MAX_OPTIONS`=64
+  - Launch stats: 64 blocks of 128 threads 
+  - Memory footprint: 1.5 KB
+  - Memory traffic: 19.33 KB
+  - fp operation: 406975744
+  - arithmetic intensity: 21264.7004
+  - type: compute-bound
 
 
 - `${CUDA_HOME}/samples/4_Finance/BlackScholes`
@@ -46,9 +63,7 @@ In this document, the specifications of all benchmarks used in this system are d
 
 
 - `${CUDA_HOME}/samples/6_Advanced/scan`: `scanExclusiveShared`, `scanExclusiveShared2`, `uniformUpdate`
-
   - data size: 208 arrays of 262144 elements
-
   - Launch stats: 53248 blocks of 256 threads
   - Memory footprint: 416.25 MB
   - Memory traffic: 433.04 MB + 3.50 MB + 435.85 MB = 872.39 MB
---
+++ mybenchmark/binomialOptions_64/binomialOptions.cpp
@@ -0,0 +1,131 @@
+/*
+ * Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+
+/*
+ * This sample evaluates fair call price for a
+ * given set of European options under binomial model.
+ * See supplied whitepaper for more explanations.
+ */
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <cuda_runtime.h>
+
+#include <helper_functions.h>
+#include <helper_cuda.h>
+
+#include "binomialOptions_common.h"
+#include "realtype.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// Black-Scholes formula for binomial tree results validation
+////////////////////////////////////////////////////////////////////////////////
+extern "C" void BlackScholesCall(
+    real &callResult,
+    TOptionData optionData
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// Process single option on CPU
+// Note that CPU code is for correctness testing only and not for benchmarking.
+////////////////////////////////////////////////////////////////////////////////
+extern "C" void binomialOptionsCPU(
+    real &callResult,
+    TOptionData optionData
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// Process an array of OptN options on GPU
+////////////////////////////////////////////////////////////////////////////////
+extern "C" void binomialOptionsGPU(
+    real *callValue,
+    TOptionData  *optionData,
+    int optN
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// Helper function, returning uniformly distributed
+// random float in [low, high] range
+////////////////////////////////////////////////////////////////////////////////
+real randData(real low, real high)
+{
+    real t = (real)rand() / (real)RAND_MAX;
+    return ((real)1.0 - t) * low + t * high;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Main program
+////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+    printf("[%s] - Starting...\n", argv[0]);
+
+    int devID = findCudaDevice(argc, (const char **)argv);
+
+    const int OPT_N = MAX_OPTIONS;
+
+    TOptionData optionData[MAX_OPTIONS];
+    real
+    callValueBS[MAX_OPTIONS],
+                callValueGPU[MAX_OPTIONS],
+                callValueCPU[MAX_OPTIONS];
+
+    real
+    sumDelta, sumRef, gpuTime, errorVal;
+
+    StopWatchInterface *hTimer = NULL;
+    int i;
+
+    sdkCreateTimer(&hTimer);
+
+    printf("Generating input data...\n");
+    //Generate options set
+    srand(123);
+
+    for (i = 0; i < OPT_N; i++)
+    {
+        optionData[i].S = randData(5.0f, 30.0f);
+        optionData[i].X = randData(1.0f, 100.0f);
+        optionData[i].T = randData(0.25f, 10.0f);
+        optionData[i].R = 0.06f;
+        optionData[i].V = 0.10f;
+        BlackScholesCall(callValueBS[i], optionData[i]);
+    }
+
+    printf("Running GPU binomial tree...\n");
+    checkCudaErrors(cudaDeviceSynchronize());
+    sdkResetTimer(&hTimer);
+    sdkStartTimer(&hTimer);
+
+    binomialOptionsGPU(callValueGPU, optionData, OPT_N);
+
+    checkCudaErrors(cudaDeviceSynchronize());
+    sdkStopTimer(&hTimer);
+    gpuTime = sdkGetTimerValue(&hTimer);
+    printf("Options count            : %i     \n", OPT_N);
+    printf("Time steps               : %i     \n", NUM_STEPS);
+    printf("binomialOptionsGPU() time: %f msec\n", gpuTime);
+    printf("Options per second       : %f     \n", OPT_N / (gpuTime * 0.001));
+
+    printf("Shutting down...\n");
+
+    sdkDeleteTimer(&hTimer);
+
+    printf("\nNOTE: The CUDA Samples are not meant for performance measurements. Results may vary when GPU Boost is enabled.\n\n");
+
+    exit(EXIT_SUCCESS);
+}
